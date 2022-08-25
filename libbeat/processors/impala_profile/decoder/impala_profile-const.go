0ca275fac1c218a84ae69d4be945244e820b8c76
xiangli <believems@qq.com>
2022-08-25 11:36:39 +0800

    增加impala_profile日志处理的processor支持

---
+++ libbeat/processors/impala_profile/decoder/impala_profile-const.go
@@ -0,0 +1,43 @@
+package decoder
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/elastic/elastic-agent-libs/mapstr"
+	"time"
+)
+
+type ImpalaProfile struct {
+	Timestamp time.Time `json:"timestamp"`
+	QueryId   string    `json:"queryId"`
+	Profile   string    `json:"profile"`
+}
+
+func (profile *ImpalaProfile) StringMap() (mapstr.M, error) {
+	var stringMap mapstr.M
+	data, err := json.Marshal(profile)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(data, &stringMap)
+	if err != nil {
+		return nil, err
+	}
+	return stringMap, nil
+}
+
+func (profile *ImpalaProfile) MarshalJSON() ([]byte, error) {
+	type Alias ImpalaProfile
+	return json.Marshal(&struct {
+		Timestamp string `json:"timestamp"`
+		*Alias
+	}{
+		Timestamp: profile.Timestamp.Format(time.RFC3339),
+		Alias:     (*Alias)(profile),
+	})
+}
+
+func (profile *ImpalaProfile) String() string {
+	timeStr := profile.Timestamp.Format(time.RFC3339)
+	return fmt.Sprintf("%s %s %s", timeStr, profile.QueryId, profile.Profile)
+}
