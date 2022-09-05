ed2926e6182a69f39cae611837dbafc421347bfb
xiangli <believems@qq.com>
2022-09-05 16:03:33 +0800

    增加impala相关4个module

--- filebeat/include/list.go
+++ filebeat/include/list.go
@@ -35,6 +35,7 @@ import (
 	_ "github.com/elastic/beats/v7/filebeat/module/haproxy"
 	_ "github.com/elastic/beats/v7/filebeat/module/icinga"
 	_ "github.com/elastic/beats/v7/filebeat/module/iis"
+	_ "github.com/elastic/beats/v7/filebeat/module/impala"
 	_ "github.com/elastic/beats/v7/filebeat/module/kafka"
 	_ "github.com/elastic/beats/v7/filebeat/module/kibana"
 	_ "github.com/elastic/beats/v7/filebeat/module/logstash"
